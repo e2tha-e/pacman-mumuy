2b3a177ce9d93ea5639d91b2429a801851accd9d
electric-el <electric-el@electric-eloquence.io>
2021-08-18 07:48:54 -0400

    updated electron.js file

--- electron.js
+++ electron.js
@@ -8,8 +8,6 @@ var win;
 app.on('ready', function() {
   win = new BrowserWindow({ width: 768, height: 540 });
   win.webContents.session.clearCache(function() {
-    win.webContents.session.clearStorageData({}, function() {
-      win.loadURL(`file:///${__dirname}/index.html`);
-    });
+    win.loadURL(`file://${__dirname}/index.html`);
   });
 });
